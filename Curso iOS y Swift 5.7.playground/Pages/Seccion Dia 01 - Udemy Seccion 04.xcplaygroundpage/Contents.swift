bd09466ae3912b6c6f46d6514408c0f757b3eabf
Oswaldo Escobar <oswaldocompany@gmail.com>
2024-01-02 22:31:48 -0500

    Update Dia 01
    e llegado hasta asignacion de variables

--- Curso iOS y Swift 5.7.playground/Pages/Seccion Dia 01 - Udemy Seccion 04.xcplaygroundpage/Contents.swift
+++ Curso iOS y Swift 5.7.playground/Pages/Seccion Dia 01 - Udemy Seccion 04.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-import UIKit
+
 import Foundation
 
 /*:
@@ -6,37 +6,7 @@ import Foundation
 */
 
 // Constante
-let numberOfItems = 10
+let numeroConstante = 10
 // Variable
-var isButtonTapped = true
-
-let numberOfData: Int = 10
-var isImageTapped: Bool = false
-
-// Definir varias variables en una misma línea
-var coordinateX = 0.0, coordinateY = 0.0, coordinateZ = 0.0
-
-// Anotaciones de tipo para especificar el tipo exacto que queremos
-var myDouble = 1.2
-
-// Ahora queremos que 1.2 sea de tipo CGFloat, por lo tanto se lo debemos especificar. Para usar CGFloat debemos importar UIKit
-var myCGFloat: CGFloat = 1.2
-
-/*:
- ### Type Aliases
-*/
-
-typealias Name = String
-var name: Name = "Swift"
-
-/*:
- ### Tuplas
-*/
-
-let user = ("Swift", "Helo")
-user.0
-user.1
-
-let (brandName, brandSurname) = user
-
-//: [Next](@next)
+var numeroVariable = true
+print("hello world")
